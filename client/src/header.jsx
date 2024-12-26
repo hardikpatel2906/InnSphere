622e15d551d2de2c067b54d8296b17e3b85cb755
Hardik Patel <h.patel290695@gmail.com>
2024-12-25 23:36:01 -0500

    Initial commit / Added Layout

--- client/src/header.jsx
+++ client/src/header.jsx
@@ -6,9 +6,9 @@ const Header = () => {
     return (
         <header className="flex justify-between">
             <Link to={'/'} className="flex item-center gap-1">
-            <span className="font-bold text-3xl">
-                <span className="text-[#0065FF]">InnSphere</span>
-            </span>
+                <span className="font-bold text-3xl">
+                    <span className="text-[#0065FF]">InnSphere</span>
+                </span>
             </Link>
         </header>
     );
