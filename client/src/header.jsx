f7d0d847711473a52c6a1d31e379f47746d63b2b
Hardik Patel <h.patel290695@gmail.com>
2024-12-25 23:34:33 -0500

    Initial commit / Added Layout

---
+++ client/src/header.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+
+
+const Header = () => {
+    return (
+        <header className="flex justify-between">
+            <Link to={'/'} className="flex item-center gap-1">
+            <span className="font-bold text-3xl">
+                <span className="text-[#0065FF]">InnSphere</span>
+            </span>
+            </Link>
+        </header>
+    );
+}
+export default Header;
